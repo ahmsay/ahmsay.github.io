f95877a399c638647456640bb80fdd3f5aeda765
ahmsay <ahmsay@gmail.com>
2020-04-26 12:38:59 +0300

    initial c

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello There</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
