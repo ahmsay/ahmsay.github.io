682df79ccebc726277e1b0559de9577cf0986d66
ahmsay <ahmsay@gmail.com>
2020-04-30 22:25:53 +0300

    withStyles added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,25 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import { connect } from 'react-redux'
 import { changeActiveTab } from '../actions/tabActions'
+import { withStyles } from '@material-ui/core/styles'
+
+const useStyles = (theme) => ({
+  root: { flexGrow: 1 },
+  menuButton: { marginRight: theme.spacing(2) },
+  title: { flexGrow: 1 }
+})
 
 class Navbar extends Component {
   changeTab = (id) => {
     this.props.changeTab(id)
   }
   render() {
+    const { classes } = this.props
     return (
-      <div>
+      <div className={ classes.root }>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6">
+            <Typography variant="h6" className={ classes.title }>
               This site is under construction.
             </Typography>
             <Button onClick={ () => this.changeTab(1) } color="inherit">Tab1</Button>
@@ -37,4 +45,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(Navbar)
+export default connect(null, mapDispatchToProps)(withStyles(useStyles)(Navbar))
