7c0c8e79404f65d339d730b4bad30d307f701ede
ahmsay <ahmsay@gmail.com>
2020-05-04 10:27:14 +0300

    about page content externalized

--- src/components/tabs/About.jsx
+++ src/components/tabs/About.jsx
@@ -12,49 +12,29 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-const About = () => {
+const About = ({ tab }) => {
   const classes = useStyles()
+  const { mainTitle, mainContent, section1, section2, section3 } = tab.card
+  const links = section3.links.map(link => {
+    return (
+      <li key={ link.name }>
+        <Link href={ link.href } target="_blank" rel="noopener" className={ classes.link }>
+          { link.name }
+        </Link>
+      </li>
+    )
+  })
   return (
-    <Card className={classes.card} variant="outlined">
+    <Card className={ classes.card } variant="outlined">
       <CardContent>
-        <Typography variant="h5" component="h2">
-          Hello There !
-        </Typography><br/>
-        <Typography variant="body2" component="p">
-          My name is Ahmet. I was born in Eskişehir. I am graduated from Anadolu University and living in Ankara, Turkey. I work at TÜBİTAK (Scientific and Technological Research Council of Turkey) as a software developer. I am interested in devops and microservices architecture.
-        </Typography><br/>
-        <Typography variant="subtitle1" component="h2">
-          What Else ?
-        </Typography>
-        <Typography variant="body2" component="p">
-          I like go kart, watching movies and tv series, listening eurobeat, playing video games, table football, billiard, drawing, drinking tea. I am really interested in space related topics and wild nature. Tiger is my favorite animal. Besides work, I also code some stuff to learn and improve myself.
-        </Typography><br/>
-        <Typography variant="subtitle1" component="h2">
-          For Those Who Wants to be a Software Developer
-        </Typography>
-        <Typography variant="body2" component="p">
-          There are tons of materials on internet that can guide you, help you to solve the problem you stuck and improve your skills. All you need is a desire to learn. I know this sounds like a cliche but it is the truth.
-        </Typography><br/>
-        <Typography variant="body1">
-          For More
-          <ul>
-            <li>
-              <Link href="https://tr.linkedin.com/in/ahmet-say-769485120" target="_blank" rel="noopener" className={ classes.link }>
-                LinkedIn
-              </Link>
-            </li>
-            <li>
-              <Link href="https://github.com/ahmsay" target="_blank" rel="noopener" className={ classes.link }>
-                GitHub
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.instagram.com/ahmsay26/" target="_blank" rel="noopener" className={ classes.link }>
-                Instagram
-              </Link>
-            </li>
-          </ul>
-        </Typography>
+        <Typography variant="h5">{ mainTitle }</Typography><br/>
+        <Typography variant="body2">{ mainContent }</Typography><br/>
+        <Typography variant="subtitle1">{ section1.title }</Typography>
+        <Typography variant="body2">{ section1.content }</Typography><br/>
+        <Typography variant="subtitle1">{ section2.title }</Typography>
+        <Typography variant="body2">{ section2.content }</Typography><br/>
+        <Typography variant="subtitle1">{ section3.title }</Typography>
+        <ul>{ links }</ul>
       </CardContent>
     </Card>
   )
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,9 +5,24 @@ const initState = {
       id: 1,
       title: 'About Me',
       card: {
-        title: 'About Me',
-        content1: 'tab 1 content',
-        content2: 'tab 1 more content'
+        mainTitle: 'Hello There !',
+        mainContent: 'My name is Ahmet. I was born in Eskişehir. I am graduated from Anadolu University and living in Ankara, Turkey. I work at TÜBİTAK (Scientific and Technological Research Council of Turkey) as a software developer. I am interested in devops and microservices architecture.',
+        section1: {
+          title: 'What Else ?',
+          content: 'I like go kart, watching movies and tv series, listening eurobeat, playing video games, table football, billiard, drawing, drinking tea. I am really interested in space related topics and wild nature. Tiger is my favorite animal. Besides work, I also code some stuff to learn and improve myself.'
+        },
+        section2: {
+          title: 'For Those Who Wants to be a Software Developer',
+          content: 'There are tons of materials on internet that can guide you, help you to solve the problem you stuck and improve your skills. All you need is a desire to learn. I know this sounds like a cliche but it is the truth.'
+        },
+        section3: {
+          title: 'For More',
+          links: [
+            { name: 'LinkedIn', href: 'https://tr.linkedin.com/in/ahmet-say-769485120' },
+            { name: 'GitHub', href: 'https://github.com/ahmsay' },
+            { name: 'Instagram', href: 'https://www.instagram.com/ahmsay26/' }
+          ]
+        }
       }
     },
     {
