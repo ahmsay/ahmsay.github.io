ad4a486b9594a79057433e981f73634ca2ec3e5c
ahmsay <ahmsay@gmail.com>
2020-08-22 12:09:41 +0300

    functionality of getting window width and height added

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
 
-class Tablist extends Component {
+class Greetings extends Component {
+  constructor() {
+    super();
+    this.state = {asd: 'lol'};
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  componentDidMount() {
+      this.updateWindowDimensions();
+      window.addEventListener("resize", this.updateWindowDimensions.bind(this));
+  }
+  componentWillUnmount() {
+      window.removeEventListener("resize", this.updateWindowDimensions.bind(this));
+  }
+  updateWindowDimensions() {
+      this.setState({width: window.innerWidth, height: window.innerHeight });
+  }
   render() {
     return (
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: 500}}>
-        Hello there
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: this.state.height}}>
+        Hello there<br/>
       </div>
     )
   }
 }
 
-export default Tablist
+export default Greetings
