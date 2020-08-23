2dce50a4095784a631898fa943f688afa8b2e4a9
ahmsay <ahmsay@gmail.com>
2020-08-23 13:08:14 +0300

    expansion panel is removed from projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,96 +1,42 @@
 import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import { CardActions, ExpansionPanelActions, Button, Card, CardContent, Typography } from '@material-ui/core'
-import MuiExpansionPanel from '@material-ui/core/ExpansionPanel'
-import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
-import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { CardActions, Button, Card, CardContent, Typography, Grid } from '@material-ui/core'
 
 const useStyles = (theme) => ({
   card: theme.card
 })
 
-const ExpansionPanel = withStyles({
-  root: {
-    backgroundColor: '#0001',
-    color: '#000',
-    border: '1px solid rgba(0, 0, 0, .125)',
-    boxShadow: 'none',
-    '&:not(:last-child)': {
-      borderBottom: 0
-    },
-    '&:before': {
-      display: 'none'
-    },
-    '&$expanded': {
-      margin: 'auto'
-    }
-  },
-  expanded: {}
-})(MuiExpansionPanel)
-
-const ExpansionPanelSummary = withStyles({
-  root: {
-    backgroundColor: '#fff2',
-    borderBottom: '1px solid rgba(0, 0, 0, .125)',
-    marginBottom: -1,
-    minHeight: 56,
-    '&$expanded': {
-      minHeight: 56
-    }
-  },
-  content: {
-    '&$expanded': {
-      margin: '12px 0'
-    }
-  },
-  expanded: {}
-})(MuiExpansionPanelSummary)
-
-const ExpansionPanelDetails = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2)
-  }
-}))(MuiExpansionPanelDetails)
-
 class Projects extends Component {
-  state = {
-    expandedPanelIndex: '-',
-    newExpanded: false
-  }
+  state = {}
   render() {
     const { classes } = this.props
     const { mainTitle, projectList } = this.props.projectsContent
-    const handleChange = (index) => (event, newExpanded) => {
-      this.setState({
-        expandedPanelIndex: index,
-        newExpanded
-      })
-    }
     const projectListProcessed = projectList.map((project, index) => {
       return (
-        <ExpansionPanel key={ index } square expanded={ this.state.expandedPanelIndex === index && this.state.newExpanded } onChange={ handleChange(index) }>
-          <ExpansionPanelSummary expandIcon={ <ExpandMoreIcon style={{ color: '#000' }}/> }>
-            <Typography variant="subtitle1">
-              { project.title }
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography variant="body2">
-              { project.detail }
-            </Typography>
-          </ExpansionPanelDetails>
-          <ExpansionPanelActions style={{ paddingTop: 0 }}>
-            <Button onClick={ () => { window.open( project.link, "_blank") } } size="small">More</Button>
-          </ExpansionPanelActions>
-        </ExpansionPanel>
+        <Grid item xs={12} sm={6} md={3} key={ index }>
+          <Card>
+            <CardContent>
+              <Typography variant="subtitle1">
+                { project.title }
+              </Typography>
+              <Typography variant="body2">
+                { project.detail }
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button onClick={ () => { window.open( project.link, "_blank") } } size="small">More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
       )
     })
     return (
       <Card className={ classes.card } variant="outlined" align="justify">
         <CardContent>
           <Typography variant="h5">{ mainTitle }</Typography><br/>
-          { projectListProcessed }
+          <Grid container spacing={3}>
+            { projectListProcessed }
+          </Grid>
         </CardContent>
         <CardActions style={{ paddingTop: 0 }}>
           <Button onClick={ () => { window.open( "https://github.com/ahmsay?tab=repositories", "_blank") } } style={{ marginLeft: 4 }} size="small">See Other Projects</Button>
