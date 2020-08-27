570a7d9cdd44289d2689b3af4ddf82ba9ccca6ba
ahmsay <ahmsay@gmail.com>
2020-08-27 19:18:13 +0300

    a little adjustment on hobbies page content

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -23,7 +23,7 @@ const initState = {
     hobbies: {
       mainTitle: 'My Hobbies',
       hobbyList: [
-        { icon: 'create', content: "Drawing is really an easy and fun activity. I always make the most score for my team when it comes to drawing in taboo. I am terrible at painting so most of my drawings are charcoal. I also like to draw technical stuff like aircrafts or space ships." },
+        { icon: 'create', content: "Drawing is really an easy and fun activity. I always make the most score for my team when it comes to drawing in taboo game. I am terrible at painting so most of my drawings are charcoal. I also like to draw technical stuff like aircrafts or space ships." },
         { icon: 'movie', content: "Everybody likes watching movies but this is something more for me. Plot and soundtrack are two things I pay attention the most. I can't really tell my favorite movie since it is so hard to decide but I can tell that Hans Zimmer is my favorite composer." },
         { icon: 'games', content: "Despite I am not playing that much compared to before, I still play video games time to time. Assassin's Creed 2 and Hollow Knight are my two favorites. I usually like open-world games with great story and soundtrack." },
         { icon: 'code', content: "Coding is not something I do only for work. I like to learn new technologies, concepts and challenge myself. I like both front-end and back-end and I have many interests like machine learning, design patterns, linux shell, devops and microservices." }
